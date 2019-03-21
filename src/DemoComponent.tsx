bbadfdd65c982bb0274977c8e021a59cd4f3524f
Jose Gonzalez <jgonzalez@credera.com>
2019-03-21 11:17:24 -0500

    changing TS lib used to show how to write .includes type for array;

--- src/DemoComponent.tsx
+++ src/DemoComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
+import * as reactD3 from 'react-d3-components';
 
 export default class extends React.Component<Props, State> {
     render() {
@@ -8,6 +9,21 @@ export default class extends React.Component<Props, State> {
         });
 
         let colorsIncludeYellow = this.props.colors.includes("Yellow");
+
+        let data = [
+            {
+                label: 'somethingA',
+                values: [{ x: 'SomethingA', y: 10 }, { x: 'SomethingB', y: 4 }, { x: 'SomethingC', y: 3 }]
+            },
+            {
+                label: 'somethingB',
+                values: [{ x: 'SomethingA', y: 6 }, { x: 'SomethingB', y: 8 }, { x: 'SomethingC', y: 5 }]
+            },
+            {
+                label: 'somethingC',
+                values: [{ x: 'SomethingA', y: 6 }, { x: 'SomethingB', y: 8 }, { x: 'SomethingC', y: 5 }]
+            }
+        ];
 
         return (
             <div>
@@ -20,6 +36,13 @@ export default class extends React.Component<Props, State> {
                         first item in list
                     </li>
                 </ol>
+
+                <reactD3.BarChart
+                    data={data}
+                    height={400}
+                    width={600}
+                    margin={{ top: 10, bottom: 50, left: 50, right: 10 }}
+                />
             </div>
         );
     }
