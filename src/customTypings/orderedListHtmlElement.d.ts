9ebeea4171161d9f9e58dc9520b8ef3b60d43638
Jose Gonzalez <jgonzalez@credera.com>
2019-03-25 18:55:05 -0500

    simplifying orderedListHtmlElement.d.ts definition

--- src/customTypings/orderedListHtmlElement.d.ts
+++ src/customTypings/orderedListHtmlElement.d.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 declare module 'react' {
-    interface OlHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    interface OlHTMLAttributes<T> {
         type?: "1" | "a" | "A" | "i" | "I";
     }
 }
